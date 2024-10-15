334215706ec1693d213058db99f46969479d0f6a
ryb2tk <ryb2tk@umsystem.edu>
2024-10-14 20:44:20 -0500

    kafka streaming data into Athena

--- week4_kafka_bronze/week4_build_bronze_layer_with_kafka.py
+++ week4_kafka_bronze/week4_build_bronze_layer_with_kafka.py
@@ -1,6 +1,6 @@
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, current_timestamp
+from pyspark.sql.functions import col, current_timestamp, split
 
 # Load environment variables.
 from dotenv import load_dotenv
@@ -18,6 +18,8 @@ username = os.environ.get("HWE_USERNAME")
 password = os.environ.get("HWE_PASSWORD")
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+class_name = "fall-2024"
+handle = "rbrandt"
 kafka_topic = "reviews"
 
 # Create a SparkSession
@@ -48,18 +50,47 @@ df = spark \
     .option("kafka.sasl.mechanism", "SCRAM-SHA-512") \
     .option("kafka.sasl.jaas.config", getScramAuthString(username, password)) \
     .load() \
-    .selectExpr("CAST(value AS STRING)") #this helps display the values so they're more readable, not just bytes
-    #.select(col("value").cast("string")) #different way of doing the selectExpr
-df.printSchema()
+    .selectExpr("CAST(value AS STRING)") \
+    .selectExpr("SPLIT(value, '\t') as split_values") \
+    .selectExpr( 
+    "split_values[0] as marketplace",
+    "split_values[1] as customer_id",
+    "split_values[2] as review_id",
+    "split_values[3] as product_id",
+    "split_values[4] as product_parent",
+    "split_values[5] as product_title",
+    "split_values[6] as product_category",
+    "split_values[7] as star_rating",
+    "split_values[8] as helpful_votes",
+    "split_values[9] as total_votes", 
+    "split_values[10] as vine",
+    "split_values[11] as verified_purchase",
+    "split_values[12] as review_headline",
+    "split_values[13] as purchase_date" 
+) \
+    .withColumn('review_timestamp', current_timestamp())
 
-# Process the received data. Note, turning off truncate lets us view the data. Also note that "show" does not work with
+  #  * append a column to the data named `review_timestamp` which is set to the current_timestamp
+
+
+  #  * write that data as Parquet files to S3 under `s3a://hwe-$CLASS/$HANDLE/bronze/reviews` using append mode and a checkpoint location of `/tmp/kafka-checkpoint`
 query = df \
   .writeStream \
-  .format("console") \
   .outputMode("append") \
-  .option("truncate", "false") \
+  .format("parquet") \
+  .option("path", f"s3a://hwe-{class_name}/{handle}/bronze/reviews") \
   .option("checkpointLocation", "C:/Users/ryche/Documents/data_engineering/tmp/kafka-checkpoint") \
   .start()
+
+# Process the received data. Note, turning off truncate lets us view the data. Also note that "show" does not work with streaming queries.
+# The below query is to troubleshoot in console.
+# query = df \
+#   .writeStream \
+#   .format("console") \
+#   .outputMode("append") \
+#   .option("checkpointLocation", "C:/Users/ryche/Documents/data_engineering/tmp/kafka-checkpoint") \
+#   .start()
+  # .option("truncate", "false") \ include this in the query to see the full message
 
 # Wait for the streaming query to finish
 query.awaitTermination()
