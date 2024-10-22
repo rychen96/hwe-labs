2b0ef761e2a759259db96bea60e44b8530b6ea31
ryb2tk <ryb2tk@umsystem.edu>
2024-10-21 19:26:34 -0500

    week5_lab complete and working

--- week5_silver/week5_build_silver_layer.py
+++ week5_silver/week5_build_silver_layer.py
@@ -10,7 +10,8 @@ load_dotenv()
 
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
+class_name = "fall-2024"
+handle = "rbrandt"
 
 # Create a SparkSession
 spark = SparkSession.builder \
@@ -29,15 +30,41 @@ logger = spark.sparkContext._jvm.org.apache.log4j
 logger.LogManager.getLogger("org.apache.spark.util.ShutdownHookManager"). setLevel( logger.Level.OFF )
 logger.LogManager.getLogger("org.apache.spark.SparkEnv"). setLevel( logger.Level.ERROR )
 
-bronze_schema = None
+bronze_reviews_schema = StructType([
+    StructField("marketplace", StringType(), nullable=True)
+    ,StructField("customer_id", StringType(), nullable=True)
+    ,StructField("review_id", StringType(), nullable=True)
+    ,StructField("product_id", StringType(), nullable=True)
+    ,StructField("product_parent", StringType(), nullable=True)
+    ,StructField("product_title", StringType(), nullable=True)
+    ,StructField("product_category", StringType(), nullable=True)
+    ,StructField("star_rating", StringType(), nullable=True)
+    ,StructField("helpful_votes", StringType(), nullable=True)
+    ,StructField("total_votes", StringType(), nullable=True)
+    ,StructField("vine", StringType(), nullable=True)
+    ,StructField("verified_purchase", StringType(), nullable=True)
+    ,StructField("review_headline", StringType(), nullable=True)
+    ,StructField("review_body", StringType(), nullable=True)
+    ,StructField("purchase_date", StringType(), nullable=True)
+    ,StructField("review_timestamp", TimestampType(), nullable=True)
+])
 
-bronze_reviews = None
+#week 4, streaming
+bronze_reviews = spark.readStream.schema(bronze_reviews_schema).parquet(f"s3a://hwe-{class_name}/{handle}/bronze/reviews")
 
-bronze_customers = None
+#week 3, static
+bronze_customers = spark.read.parquet(f"s3a://hwe-{class_name}/{handle}/bronze/customers")
 
-silver_data = None
+#join two above
+silver_data = bronze_reviews.join(bronze_customers, on="review_id", how="inner")
 
-streaming_query = None
+
+streaming_query = silver_data \
+  .writeStream \
+  .outputMode("append") \
+  .format("parquet") \
+  .option("path", f"s3a://hwe-{class_name}/{handle}/silver/reviews") \
+  .option("checkpointLocation", "C:/Users/ryche/Documents/data_engineering/tmp2/kafka-checkpoint-silver")
 
 streaming_query.start().awaitTermination()
 
